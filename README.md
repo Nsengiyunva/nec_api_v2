626d894bc133fefe534010004283e8e5c51c1596
Nsengiyunva <kingbecks07@gmail.com>
2026-07-07 10:10:43 +0300

    updates the app

---
+++ README.md
@@ -0,0 +1,7 @@
+#sudo a2dissite 000-default
+#sudo a2enmod rewrite headers deflate proxy_http
+#sudo a2ensite library
+#sudo systemctl restart apache2
+
+#admin2026NSE@!
+
--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import authRoutes from "./routes/authRoutes";
-// import payrollRoutes from "./routes/payrollRoute";
-// import { sequelize, connectDB } from "./config/database";
-// import cors from "cors";
-// import helmet from 'helmet';
-// import listEndpoints from 'express-list-endpoints';
-
-// dotenv.config();
-
-// const app = express();
-// app.use(cors());
-// app.use(helmet.contentSecurityPolicy({
-//     directives: {
-//       connectSrc: ["'self'", "https://necapi.erb.go.ug"],
-//     }
-//   }));
-// app.use(express.json());
-
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/payroll", payrollRoutes);
-
-
-// connectDB();
-// sequelize.sync().then(() => console.log("Tables synced..."));
-
-// console.log("all routes", listEndpoints(app));
-
-// app.listen(process.env.PORT, () => console.log(`Server running on ${process.env.PORT}`));
-
-
-// server.ts
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
@@ -59,7 +25,6 @@ app.use(helmet.contentSecurityPolicy({
 
 app.use(express.json());
 
-// Proxy /api/external/* → https://necapi.erb.go.ug/api/*
 app.use(
   "/api/external",
   createProxyMiddleware({
